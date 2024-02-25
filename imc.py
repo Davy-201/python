775620802c5f0fafe2659f2de6cb67cbfea471b1
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-02-25 16:21:05 -0300

    aula

--- imc.py
+++ imc.py
@@ -3,4 +3,4 @@ peso = float(input('digite aqui seu peso: '))
 idade = int(input('digite aqui sua idade: '))
 
 imc = peso / (altura*altura)
-print(f1.90"O seu imc é: {imc:.2f}")
+print(f"O seu imc é: {imc:.2f}")
