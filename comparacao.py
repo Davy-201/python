75ed53176b6add7b7048f13720170e3401ad8c80
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-02-25 16:39:01 -0300

    aula

---
+++ comparacao.py
@@ -0,0 +1,5 @@
+num = int(input('digite um valor'))
+comparacao = int(input('digite outro valor'))
+
+resultado = 2 * num + 3
+print(f'O resultado: {resultado} valor da comparação: {resultado >= comparacao}')
