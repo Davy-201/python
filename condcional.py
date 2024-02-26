f964b6090866527f0e581d36fecac51648bfeec6
Davy-201 <123582957+Davy-201@users.noreply.github.com>
2024-02-25 22:14:08 -0300

    aula

---
+++ condcional.py
@@ -0,0 +1,12 @@
+nota = float(input('digite a nota do aluno'))
+
+if(nota >= 9):
+    print('conceito final: A')
+if(nota >= 7):
+    print('conceito final: B')
+if(nota >= 6):
+    print('conceito final: C')
+if(nota >= 5):
+    print('conceito final: D')
+else:
+    print('conceito final: F')
--- usodeif.py
+++ usodeif.py
@@ -3,7 +3,7 @@ if (idade >= 18): print ('você é maior de idade')
 if (idade < 18): print ('você é menor de idade')
 
 print('Responda a pergunta a seguir com sim ou não')
-resposta = input('Seu time venceu a ultima partida que disputou ?')
+resposta = input('Seu time venceu a ultima partida que disputou ? \n')
 
 if ((resposta == 'Sim') or (resposta == 'sim')):
     print ('Que ótima notícia para você')
